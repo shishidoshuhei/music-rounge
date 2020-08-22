80f53d7186ff4cdd5525dab3fcc011bb9e6ecebc
vagrant <vagrant@localhost.localdomain>
2020-08-22 07:04:12 +0000

    コメント機能の追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,17 @@
 class CommentsController < ApplicationController
   def create
-  	post = Post.find(params[:post.id])
-  	comment = current_user.post.new(post_params)
-  	comment.post_id = post.id
+  	post = Post.find(params[:post_id])
+  	comment = post.comments.new(comment_params)
+  	comment.user_id = current_user.id
   	comment.save
-  	redirect_to post.path(post.id)
+  	redirect_to post_path(post.id)
   end
 
   def destroy
   end
 
   private
-  def post_params
-  	params.require(:post).require(:body)
+  def comment_params
+  	params.require(:comment).permit(:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get 'users/show'
   root to: 'posts#index'
-  resources :posts
   devise_for :users
   resources :users, only: [:show]
+  resources :posts do
+    resources :comments, only: [:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
