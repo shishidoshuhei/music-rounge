eb0a629fcea71ec8a59fa885e7b224fffbd7ed6d
vagrant <vagrant@localhost.localdomain>
2020-08-25 03:00:31 +0000

    いいね機能の追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,8 @@ class Post < ApplicationRecord
   has_many :favorites
   has_many :post_tag
   has_many :users, through: :favorites
+
+  def favorited_by?(user)
+  	favorites.where(user_id: user.id).exists?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   resources :posts do
+    resource :favorites, only: [:create, :destroy]
     resources :comments, only: [:create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
